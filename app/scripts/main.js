9a04e3f877b4c28bd0cb52ef52e24dde93b2dba1
James Lane <lanejames35@gmail.com>
2019-04-25 10:11:35 -0400

    First attempt with Vue2Leaflet

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,21 @@
 //import Vue from 'vue'
 //import axios from 'axios'
 
+Vue.component('l-map', window.Vue2Leaflet.LMap);
+Vue.component('l-tile-layer', window.Vue2Leaflet.LTileLayer);
+
 const app = new Vue({
   el: '#app',
   data: {
     features: [],
     indicators: [],
-    selected: ''
+    selected: '',
+    center: [43.9073, -78.9560],
+    zoom: 10,
+    url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
   },
-  mounted(){
+  created(){
     axios.get('https://opendata.arcgis.com/datasets/29e0d68ac7234bb4b36fa4faf657fa01_29.geojson')
         .then(response => {
           this.features = response.data.features;
@@ -24,8 +31,7 @@ const app = new Vue({
   <div>
     <select v-model="selected">
       <option v-for="indicator in indicators"
-              v-bind:value="indicator"
-      >
+              v-bind:value="indicator" >
         {{ indicator }}
       </option>
     </select>
@@ -39,6 +45,15 @@ const app = new Vue({
         </td>
       </tr>
     </table>
+    <l-map
+      :zoom="zoom"
+      :center="center"
+      style="height: 400px" >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution" >
+      </l-tile-layer>
+    </l-map>
   </div>
   `,
 });
